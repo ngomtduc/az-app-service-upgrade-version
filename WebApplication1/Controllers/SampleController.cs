cb2e4d23bbd64a879112539fba1548010771aca5
ThienNguyenTriBuu <thien.nguyentribuu@petronas.com>
2023-11-30 13:30:16 +0700

    add code net fw 4.8

---
+++ WebApplication1/Controllers/SampleController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class SampleController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello. This API run on .Net Framework 4.8";
+        }
+    }
+}
